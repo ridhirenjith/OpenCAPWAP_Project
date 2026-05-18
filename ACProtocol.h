1179078a702173ab0bd8a7b3e117919a7b610666
Ridhi <ridhi@gmail.com>
2026-05-18 18:57:37 +0530

    Revert "fix: remove all compiler warnings from OpenCAPWAP project"
    This reverts commit f7aa7d13e81ab4c162534ab87ad6e31633ae3689.

--- ACProtocol.h
+++ ACProtocol.h
@@ -237,5 +237,4 @@ CWBool CWACSupportIPv6();
 void CWDestroyDiscoveryRequestValues(CWDiscoveryRequestValues *valPtr);
 
 CWBool CWProtocolAssembleConfigurationUpdateRequest(CWProtocolMessage **msgElems, int *msgElemCountPtr, int MsgElementType);
-int CWACGetECNSupport(void);
 #endif
